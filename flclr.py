8a2b43e64ab8509113eebe70d6546f0fa7963961
Silpa S Achharya <117834398+Silpa-S-Acharya@users.noreply.github.com>
2022-12-05 23:55:52 +0530

    Update flclr.py

--- flclr.py
+++ flclr.py
@@ -1,2 +1,10 @@
 clr=["Green","Red","White"]
 print("First and Last colors from list are ",clr[0],clr[-1])
+'''n=int(input("Enter the size of the list "))
+for i in range(0,n):
+    print("\nEnter the ", i+1," clour in a list")
+    elm=input()
+    clr.append(elm)
+print("First and Last colors from list are ",clr[0],clr[-1])
+print("\nThe first colour is ",clr[0])
+print("\nThe last colour is ",clr[-1])'''

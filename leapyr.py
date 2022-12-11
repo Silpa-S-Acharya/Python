8eadb04ff445987d28c0f46edbf9af38b77a79d1
Silpa S Achharya <117834398+Silpa-S-Acharya@users.noreply.github.com>
2022-12-11 19:46:52 +0530

    Delete leapyr.py

--- leapyr.py
+++
@@ -1,9 +0,0 @@
-year=int(input("Enter the current year\t"))
-fut=int(input("Enter the future year\t"))
-print("The leap years in range are\n" )
-for year in range(year,fut+1):
-    if year%4==0 and year%100!=0 or year%4==0:
-        print(year)
-
-
-
